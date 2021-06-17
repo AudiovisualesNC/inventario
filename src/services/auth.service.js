298691ef057aff3cdeffdeee43c8b2cdbe6284cc
Javier <javier.sanchez.abia.contractor@bbva.com>
2021-06-17 19:16:52 +0200

    Inventario

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/';
+const API_URL = 'https://api.avserver.ddnsfree.com/api/';
 
 class AuthService {
     login(formData) {
--- src/services/room.service.js
+++ src/services/room.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8000/rooms';
+const API_URL = 'https://api.avserver.ddnsfree.com/api/rooms/';
 
 class RoomService {
     getAll() {
